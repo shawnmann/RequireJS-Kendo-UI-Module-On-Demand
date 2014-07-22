6effb12c172f0c22c9d08a9f27d2683940aab9ac
shawnmann <shawn.mann@gmail.com>
2014-07-22 07:38:04 -0700

    Extra comments, etc.

---
+++ module01.js
@@ -0,0 +1,22 @@
+﻿// Create a RequireJS module
+define([
+	'kendo/kendo.menu.min'  // Require Kendo UI's menu file, because we load it in this module
+], function (kendoMenu) {
+
+	// Declare our initModule function variable
+	var initModule;
+
+	// A function we run when the module loads
+	initModule = function () {
+		// Do something so we know the module loaded
+		console.log('MODULE01 LOADED');
+
+		// Turn our menu markup into a Kendo UI menu
+		$('#menu').kendoMenu();
+	};
+
+	// Expose our initModule as a public function
+	return {
+		initModule: initModule
+	};
+});
---
+++ module02.js
@@ -0,0 +1,15 @@
+﻿// See module01 for comments
+
+define([
+], function () {
+
+	var initModule;
+
+	initModule = function () {
+		console.log('MODULE02 LOADED');
+	};
+
+	return {
+		initModule: initModule
+	};
+});
